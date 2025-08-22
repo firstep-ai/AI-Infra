30a6412d7e47aea554fcfe2d34bce949aaae51d4
汪志鹏 <wangzhipeng628@gmail.com>
2025-08-22 17:36:03 +0800

    Create Unit_Text_Failed_Fix.py

---
+++ vLLM/donut-model/Unit_Text_Failed_Fix.py
@@ -0,0 +1,6 @@
+# pytest --pdb -sv models/multimodal/processing/test_common.py::test_processing_correctness[1.0-32-0.3-naver-clova-ix/donut-base-finetuned-docvqa]
+# 运行第二个失败的测试
+#pytest --pdb -sv models/multimodal/processing/test_common.py::test_processing_correctness[1.0-32-0.5-naver-clova-ix/donut-base-finetuned-docvqa]
+
+# 运行第三个失败的测试
+#pytest --pdb -sv models/multimodal/processing/test_common.py::test_processing_correctness[1.0-32-1.0-naver-clova-ix/donut-base-finetuned-docvqa]
