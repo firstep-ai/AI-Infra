3126042bd9ff882cd0f21b1787c89101c8771eee
汪志鹏 <wangzhipeng628@gmail.com>
2025-08-25 17:44:12 +0800

    xxx

--- vLLM/mistral3.2/test.py
+++ vLLM/mistral3.2/test.py
@@ -4,15 +4,14 @@ from vllm.assets.image import ImageAsset
 from vllm.multimodal.image import convert_image_mode
 
 if __name__ == "__main__":
-    llm = LLM(
-        # model="mistralai/Mistral-Small-3.1-24B-Instruct-2503",
-        model="mistralai/Mistral-Small-3.2-24B-Instruct-2506",
+    # model_name = "mistralai/Mistral-Small-3.1-24B-Instruct-2503"
+    model_name = "mistralai/Mistral-Small-3.2-24B-Instruct-2506"
+    llm = LLM(model=model_name,
+        max_model_len=8192,
+        max_num_seqs=2,
         tensor_parallel_size=2,
-        trust_remote_code=True,
-        tokenizer_mode="mistral",
-        config_format="mistral",
-        load_format="mistral"
-    )
+        limit_mm_per_prompt={"image": 1},
+        ignore_patterns=["consolidated.safetensors"],)
     sampling_params = SamplingParams(temperature=0.1, top_p=0.9, max_tokens=500)
 
     # 场景 1: 纯文本测试
@@ -31,31 +30,3 @@ if __name__ == "__main__":
     outputs = llm.generate(vllm_inputs_single_image, sampling_params) # 直接生成
     for output_item in outputs:
         print(f"生成: {output_item.outputs[0].text}\n" + "-" * 20)
-
-# import requests
-# import json
-
-# url = "http://localhost:8000/v1/chat/completions"
-# headers = {'Content-Type': 'application/json'}
-# data = {
-#     "model": "mistralai/Mistral-Small-3.2-24B-Instruct-2506",
-#     "messages": [
-#         {"role": "user", "content": [
-#         {"type": "image_url", "image_url": {"url": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/500px-Cat_November_2010-1a.jpg"}},
-#         {"type": "image_url", "image_url": {"url": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/020_The_lion_king_Snyggve_in_the_Serengeti_National_Park_Photo_by_Giles_Laurent.jpg/500px-020_The_lion_king_Snyggve_in_the_Serengeti_National_Park_Photo_by_Giles_Laurent.jpg"}},
-#         {"type": "text", "text": "What are the animals in these images ?"}
-#     ]}
-#     ],
-# }
-
-# try:
-#     response = requests.post(url, headers=headers, data=json.dumps(data))
-#     response.raise_for_status()  # 如果响应状态码不是 200 OK，会抛出 HTTPError 异常
-#     print("请求成功！")
-#     print("响应内容:")
-#     print(response.json())
-# except requests.exceptions.RequestException as e:
-#     print(f"请求失败: {e}")
-#     if response is not None:
-#         print(f"响应状态码: {response.status_code}")
-#         print(f"响应内容: {response.text}")
